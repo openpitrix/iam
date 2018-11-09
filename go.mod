eea934a4bc7c7ca6cf78b592cde834c728b56df5
chai2010 <chaishushan@gmail.com>
2018-11-09 22:07:22 +0800

    refine am and im cmd

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ module openpitrix.io/iam
 
 require (
 	github.com/golang/protobuf v1.2.0
+	github.com/urfave/cli v1.20.0
 	golang.org/x/net v0.0.0-20181108082009-03003ca0c849
 	google.golang.org/grpc v1.16.0
 	rsc.io/goversion v1.2.0
--- pkg/am/main.go
+++ pkg/am/main.go
@@ -4,6 +4,79 @@
 
 package am
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+
+	"openpitrix.io/iam/pkg/version"
+)
+
 func Main() {
-	//
+	app := cli.NewApp()
+	app.Name = "am"
+	app.Usage = "am provides am service."
+	app.Version = version.GetVersionString()
+
+	app.UsageText = `am [global options] command [options] [args...]
+
+EXAMPLE:
+   am gen-config
+   am info
+   am list
+   am ping
+   am getv key
+   am serve
+   am tour`
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "config",
+			Value:  "am-config.json",
+			Usage:  "am config file",
+			EnvVar: "OPENPITRIX_AM_CONFIG",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "debug",
+			Usage:  "debug app",
+			Hidden: true,
+
+			Action: func(c *cli.Context) {
+				fmt.Println(nil)
+				fmt.Println(version.GetVersion())
+				return
+			},
+		},
+
+		{
+			Name:  "serve",
+			Usage: "run as drone service",
+			Action: func(c *cli.Context) {
+				log.Fatal("TODO")
+			},
+		},
+
+		{
+			Name:  "tour",
+			Usage: "show more examples",
+			Action: func(c *cli.Context) {
+				fmt.Println(tourTopic)
+			},
+		},
+	}
+
+	app.CommandNotFound = func(ctx *cli.Context, command string) {
+		fmt.Fprintf(ctx.App.Writer, "not found '%v'!\n", command)
+	}
+
+	app.Run(os.Args)
 }
+
+const tourTopic = `
+am gen-config
+`
