6505442af96dc0bb4a4d12ad18df67db93653855
chai2010 <chaishushan@gmail.com>
2018-11-09 21:15:06 +0800

    Revert "refactoring dir struct"
    This reverts commit 402468e86803c35d91c3ce37173c9503fbc991d4.

--- cmd/am/main.go
+++ cmd/am/main.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a Apache license
 // that can be found in the LICENSE file.
 
-// Identity Management System for OpenPitrix.
 package main
 
 func main() {
--- doc.go
+++ doc.go
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a Apache license
 // that can be found in the LICENSE file.
 
-module openpitrix.io/iam/am
+// Identity and Access Management System for OpenPitrix.
+package iam
