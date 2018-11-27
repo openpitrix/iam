92ccb37f936fc8bd0d51849ec08b8e4e80b9ae69
chai2010 <chaishushan@gmail.com>
2018-11-28 05:47:53 +0800

    pkg/am/rabc: refine code

--- pkg/am/rabc/rabc_file.go
+++ pkg/am/rabc/rabc_file.go
@@ -5,57 +5,127 @@
 package rabc
 
 import (
+	"os"
+	"strings"
 	"sync"
 
+	"openpitrix.io/iam/pkg/internal/copyutil"
+	"openpitrix.io/iam/pkg/internal/jsonutil"
 	"openpitrix.io/iam/pkg/pb/am"
 )
 
-type rabcFileServer struct {
-	bindings []pbam.RoleBinding
-	roles    []pbam.Role
+type rabcJsonData struct {
+	Bindings []pbam.RoleBinding
+	Roles    []pbam.Role
+}
 
+type rabcFileServer struct {
+	jsonpath string
+	rabcJsonData
 	sync.Mutex
 }
 
 func openFileServer(jsonpath string) (Interface, error) {
-	return new(rabcFileServer), nil
+	var d rabcJsonData
+	if err := jsonutil.Load(jsonpath, &d); err != nil {
+		return nil, err
+	}
+
+	var p = &rabcFileServer{
+		jsonpath:     jsonpath,
+		rabcJsonData: d,
+	}
+	return p, nil
 }
 
 func (p *rabcFileServer) Close() error {
+	p.Lock()
+	defer p.Unlock()
+
+	var bakname = func() string {
+		if strings.HasSuffix(p.jsonpath, ".json") {
+			return p.jsonpath[:len(p.jsonpath)-len(".json")] + ".bak.json"
+		} else {
+			return p.jsonpath + ".bak.json"
+		}
+	}()
+
+	os.Rename(p.jsonpath, bakname)
+	if err := jsonutil.Save(p.jsonpath, p.rabcJsonData); err != nil {
+		os.Rename(bakname, p.jsonpath)
+		return err
+	}
+
+	p.jsonpath = ""
+	p.rabcJsonData = rabcJsonData{}
 	return nil
 }
 
 func (p *rabcFileServer) CanDo(x pbam.Action) bool {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 
 func (p *rabcFileServer) AllRoles() []pbam.Role {
-	panic("TODO")
+	p.Lock()
+	defer p.Unlock()
+
+	var d rabcJsonData
+	copyutil.MustDeepCopy(&d, p.rabcJsonData)
+	return d.Roles
 }
 func (p *rabcFileServer) AllRoleBindings() []pbam.RoleBinding {
-	panic("TODO")
+	p.Lock()
+	defer p.Unlock()
+
+	var d rabcJsonData
+	copyutil.MustDeepCopy(&d, p.rabcJsonData)
+	return d.Bindings
 }
 
 func (p *rabcFileServer) GetRoleByName(name string) (role pbam.Role, ok bool) {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 func (p *rabcFileServer) GetRoleByXid(xid []string) pbam.RoleList {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 
 func (p *rabcFileServer) CreateRole(role pbam.Role) error {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 func (p *rabcFileServer) ModifyRole(role pbam.Role) error {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 func (p *rabcFileServer) DeleteRole(name string) error {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 
 func (p *rabcFileServer) CreateRoleBinding(x []pbam.RoleBinding) error {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
 func (p *rabcFileServer) DeleteRoleBinding(xid []string) error {
+	p.Lock()
+	defer p.Unlock()
+
 	panic("TODO")
 }
