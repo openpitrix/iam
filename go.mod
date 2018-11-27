28fc3e1a6484e591ab50bc1e174f90ff1a147249
chai2010 <chaishushan@gmail.com>
2018-11-28 04:25:33 +0800

    pkg/am/config: refine code

--- go.mod
+++ go.mod
@@ -5,10 +5,15 @@
 module openpitrix.io/iam
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/bmatcuk/doublestar v1.1.1
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/golang/protobuf v1.2.0
+	github.com/koding/multiconfig v0.0.0-20171124222453-69c27309b2d7
 	github.com/urfave/cli v1.20.0
 	golang.org/x/net v0.0.0-20181108082009-03003ca0c849
 	google.golang.org/grpc v1.16.0
+	gopkg.in/yaml.v2 v2.2.1 // indirect
 	rsc.io/goversion v1.2.0
 )
--- pkg/am/config/config.go
+++ pkg/am/config/config.go
@@ -5,4 +5,49 @@
 // OpenPitrix Access Management service config package.
 package config
 
-type Config struct{}
+import (
+	"fmt"
+
+	"github.com/koding/multiconfig"
+)
+
+type Config struct {
+	*Server
+}
+
+type Server struct {
+	Log   LogConfig
+	Mysql MysqlConfig
+	AM    AMConfig
+}
+
+type AMConfig struct {
+	Port     string `default:"9115"`
+	RuleFile string `default:"rabc.json"`
+}
+
+type LogConfig struct {
+	Level      string `default:"info"` // debug, info, warn, error, fatal
+	GrpcDetail bool
+}
+
+type MysqlConfig struct {
+	Host     string `default:"openpitrix-db"`
+	Port     string `default:"3306"`
+	User     string `default:"root"`
+	Password string `default:"password"`
+	Database string `default:"openpitrix"`
+	Disable  bool   `default:"false"`
+}
+
+func (m *MysqlConfig) GetUrl() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", m.User, m.Password, m.Host, m.Port, m.Database)
+}
+
+func LoadConf(path string) (*Config, error) {
+	conf := new(Server)
+	if err := multiconfig.NewWithPath(path).Load(conf); err != nil {
+		return nil, err
+	}
+	return &Config{conf}, nil
+}
