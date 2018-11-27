2547afd033a906ac633f9fd062a076d42823f1f7
chai2010 <chaishushan@gmail.com>
2018-11-28 06:44:52 +0800

    pkg/amd/rabc: add pkg doc

--- pkg/am/rabc/rabc.go
+++ pkg/am/rabc/rabc.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a Apache license
 // that can be found in the LICENSE file.
 
+// Package rabc implements rabc with local file and Database.
 package rabc
 
 import (
