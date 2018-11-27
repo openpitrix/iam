24e39847751ceca2459567a2ed2c8532ad7f94a9
chai2010 <chaishushan@gmail.com>
2018-11-27 22:51:56 +0800

    pkg/am/config: new package

---
+++ pkg/am/config/config.go
@@ -0,0 +1,8 @@
+// Copyright 2018 The OpenPitrix Authors. All rights reserved.
+// Use of this source code is governed by a Apache license
+// that can be found in the LICENSE file.
+
+// OpenPitrix Access Management service config package.
+package config
+
+type Config struct{}
