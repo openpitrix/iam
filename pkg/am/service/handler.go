d8379227c218b256ab05d0e508f2ecee2050c58e
chai2010 <chaishushan@gmail.com>
2018-12-03 14:18:28 +0800

    am: add log for every grpc method

--- pkg/am/service/handler.go
+++ pkg/am/service/handler.go
@@ -7,30 +7,44 @@ package am
 import (
 	"context"
 
+	"openpitrix.io/iam/pkg/internal/funcutil"
 	"openpitrix.io/iam/pkg/pb/am"
+	"openpitrix.io/logger"
 )
 
 func (p *Server) CreateRole(ctx context.Context, req *pbam.Role) (*pbam.Role, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) ModifyRole(ctx context.Context, req *pbam.Role) (*pbam.Role, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) DeleteRoleByName(ctx context.Context, req *pbam.String) (*pbam.Bool, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) GetRoleByName(ctx context.Context, req *pbam.String) (*pbam.Role, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) GetRoleByXidList(ctx context.Context, req *pbam.XidList) (*pbam.RoleList, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) ListRoles(ctx context.Context, req *pbam.RoleNameFilter) (*pbam.RoleList, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
@@ -39,19 +53,29 @@ func (p *Server) CreateRoleBinding(ctx context.Context, req *pbam.RoleBindingLis
 }
 
 func (p *Server) DeleteRoleBinding(ctx context.Context, req *pbam.XidList) (*pbam.Bool, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) GetRoleBindingByRoleName(ctx context.Context, req *pbam.String) (*pbam.RoleBindingList, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 func (p *Server) GetRoleBindingByXidList(ctx context.Context, req *pbam.XidList) (*pbam.RoleBindingList, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 func (p *Server) ListRoleBindings(ctx context.Context, req *pbam.RoleNameFilter) (*pbam.RoleBindingList, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
 
 func (p *Server) CanDo(ctx context.Context, req *pbam.Action) (*pbam.Bool, error) {
+	logger.Infof(ctx, funcutil.CallerName(1))
+
 	panic("TODO")
 }
