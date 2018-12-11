b10836b9715a890b3615c3def866e57326239d05
chai2010 <chaishushan@gmail.com>
2018-12-11 16:37:38 +0800

    pkg/am: add client pkg

---
+++ pkg/im/client/client.go
@@ -0,0 +1,26 @@
+// Copyright 2018 The OpenPitrix Authors. All rights reserved.
+// Use of this source code is governed by a Apache license
+// that can be found in the LICENSE file.
+
+package client
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc"
+	"openpitrix.io/iam/pkg/pb/im"
+)
+
+func DialService(host string, port int) (
+	client pbim.AccountManagerClient,
+	conn *grpc.ClientConn,
+	err error,
+) {
+	conn, err = grpc.Dial(fmt.Sprintf("%s:%d", host, port), grpc.WithInsecure())
+	if err != nil {
+		return
+	}
+
+	client = pbim.NewAccountManagerClient(conn)
+	return
+}
