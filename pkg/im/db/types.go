0f94a668302dbb23f35bf39d84db5f023630b119
chai2010 <chaishushan@gmail.com>
2018-12-11 16:13:30 +0800

    pkg/am/db: refine data types

--- pkg/im/db/types.go
+++ pkg/im/db/types.go
@@ -5,6 +5,10 @@
 package db
 
 import (
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+
 	"openpitrix.io/iam/pkg/internal/copyutil"
 	"openpitrix.io/iam/pkg/internal/jsonutil"
 	"openpitrix.io/iam/pkg/pb/im"
@@ -19,11 +23,11 @@ type User struct {
 	Description string `db:"description"`
 	Password    string `db:"password"`
 	Status      string `db:"status"`
-	ExtraJson   string `db:"extra"` // JSON map
+	XXX_Extra   string `db:"extra"` // JSON map
 
-	CreateTime string `db:"create_time"`
-	UpdateTime string `db:"update_time"`
-	StatusTime string `db:"status_time"`
+	XXX_CreateTime time.Time `db:"create_time"`
+	XXX_UpdateTime time.Time `db:"update_time"`
+	XXX_StatusTime time.Time `db:"status_time"`
 }
 
 type Group struct {
@@ -34,39 +38,67 @@ type Group struct {
 	Email       string `db:"email"`
 	Description string `db:"description"`
 	Status      string `db:"status"`
-	ExtraJson   string `db:"extra"` // JSON map
+	XXX_Extra   string `db:"extra"` // JSON map
 
-	CreateTime string `db:"create_time"`
-	UpdateTime string `db:"update_time"`
-	StatusTime string `db:"status_time"`
+	XXX_CreateTime time.Time `db:"create_time"`
+	XXX_UpdateTime time.Time `db:"update_time"`
+	XXX_StatusTime time.Time `db:"status_time"`
 }
 
 func NewUserFrom(src *pbim.User) *User {
 	var dst = new(User)
+
 	copyutil.MustDeepCopy(dst, src)
-	dst.ExtraJson = string(jsonutil.Encode(src.Extra))
+
+	dst.XXX_Extra = string(jsonutil.Encode(src.Extra))
+
+	dst.XXX_CreateTime, _ = ptypes.Timestamp(src.CreateTime)
+	dst.XXX_UpdateTime, _ = ptypes.Timestamp(src.UpdateTime)
+	dst.XXX_StatusTime, _ = ptypes.Timestamp(src.StatusTime)
+
 	return dst
 }
 
 func (src *User) ToPbUser() *pbim.User {
 	var dst = new(pbim.User)
+
 	copyutil.MustDeepCopy(dst, src)
+
 	dst.Extra = make(map[string]string)
-	jsonutil.Decode([]byte(src.ExtraJson), dst.Extra)
+	jsonutil.Decode([]byte(src.XXX_Extra), dst.Extra)
+
+	dst.CreateTime, _ = ptypes.TimestampProto(src.XXX_CreateTime)
+	dst.UpdateTime, _ = ptypes.TimestampProto(src.XXX_UpdateTime)
+	dst.StatusTime, _ = ptypes.TimestampProto(src.XXX_StatusTime)
+
 	return dst
 }
 
 func NewGroupFrom(src *pbim.Group) *Group {
 	var dst = new(Group)
+
 	copyutil.MustDeepCopy(dst, src)
-	dst.ExtraJson = string(jsonutil.Encode(src.Extra))
+
+	dst.XXX_Extra = string(jsonutil.Encode(src.Extra))
+
+	dst.XXX_CreateTime, _ = ptypes.Timestamp(src.CreateTime)
+	dst.XXX_UpdateTime, _ = ptypes.Timestamp(src.UpdateTime)
+	dst.XXX_StatusTime, _ = ptypes.Timestamp(src.StatusTime)
+
 	return dst
 }
 
 func (src *Group) ToPbGroup() *pbim.Group {
 	var dst = new(pbim.Group)
+
 	copyutil.MustDeepCopy(dst, src)
+
 	dst.Extra = make(map[string]string)
-	jsonutil.Decode([]byte(src.ExtraJson), dst.Extra)
+	jsonutil.Decode([]byte(src.XXX_Extra), dst.Extra)
+
+	dst.CreateTime, _ = ptypes.TimestampProto(src.XXX_CreateTime)
+	dst.UpdateTime, _ = ptypes.TimestampProto(src.XXX_UpdateTime)
+	dst.StatusTime, _ = ptypes.TimestampProto(src.XXX_StatusTime)
+
 	return dst
 }
