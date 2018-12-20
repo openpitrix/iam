e2244b84c34bd32a4caa34b61d51f95944ac50a7
chai2010 <chaishushan@gmail.com>
2018-12-20 14:31:49 +0800

    fix typo

--- openpitrix/pkg/internal/access_path/access_path.go
+++ openpitrix/pkg/internal/access_path/access_path.go
@@ -14,15 +14,15 @@ const (
 	DataLevel_root
 )
 
-func GetAccessPath(user_id string, groupd_path []string, level DataLevel) string {
+func GetAccessPath(user_id string, group_path []string, level DataLevel) string {
 	switch level {
 	case DataLevel_user: // group1.group2.group3:user
-		return strings.Join(groupd_path, ".") + ":" + user_id
+		return strings.Join(group_path, ".") + ":" + user_id
 	case DataLevel_isv: // group1.group2
-		return strings.Join(groupd_path, ".")
+		return strings.Join(group_path, ".")
 	case DataLevel_root: // group1
-		if len(groupd_path) > 0 {
-			return groupd_path[0]
+		if len(group_path) > 0 {
+			return group_path[0]
 		}
 	}
 	return ""
