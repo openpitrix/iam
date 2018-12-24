1e49293d8775874152f66b843a211adf78082644
chai2010 <chaishushan@gmail.com>
2018-12-24 11:31:16 +0800

    refine server

--- openpitrix/pkg/service/server.go
+++ openpitrix/pkg/service/server.go
@@ -5,20 +5,54 @@
 package service
 
 import (
+	"context"
 	"net/http"
 
 	"google.golang.org/grpc"
+	"openpitrix.io/logger"
 )
 
 type Server struct {
 	webServer  *http.Server
 	grpcServer *grpc.Server
+	db         *Database
 }
 
 func OpenServer(dbtype, dbpath string) (*Server, error) {
-	panic("TODO")
+	db, err := Open(dbtype, dbpath)
+	if err != nil {
+		logger.Criticalf(nil, "%v", err)
+	}
+
+	p := &Server{
+		db: db,
+	}
+	return p, nil
 }
 
 func (p *Server) Close() error {
-	panic("TODO")
+	var lastErr error
+
+	if p.grpcServer != nil {
+		p.grpcServer.Stop()
+		p.grpcServer = nil
+	}
+	if p.webServer != nil {
+		if err := p.webServer.Shutdown(context.Background()); err != nil {
+			lastErr = err
+		}
+		p.webServer = nil
+	}
+
+	if p.db != nil {
+		if err := p.db.Close(); err != nil {
+			lastErr = err
+			p.db = nil
+		}
+	}
+
+	if lastErr != nil {
+		return lastErr
+	}
+	return nil
 }
