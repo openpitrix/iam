e334aa4ab5cc6df5667e6150676e6f3e18e2001d
chai2010 <chaishushan@gmail.com>
2018-12-27 21:30:59 +0800

    add DBUser

---
+++ openpitrix/pkg/service/db_type_user.go
@@ -0,0 +1,80 @@
+// Copyright 2018 The OpenPitrix Authors. All rights reserved.
+// Use of this source code is governed by a Apache license
+// that can be found in the LICENSE file.
+
+package service
+
+import (
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+
+	"openpitrix.io/iam/openpitrix/pkg/pb"
+)
+
+type DBUser struct {
+	UserId      string    `db:"user_id"`
+	GroupId     string    `db:"group_id"`
+	RoleId      string    `db:"role_id"`
+	UserName    string    `db:"user_name"`
+	Position    string    `db:"position"`
+	Email       string    `db:"email"`
+	PhoneNumber string    `db:"phone_number"`
+	Password    string    `db:"password"`
+	OldPassword string    `db:"old_password"`
+	Description string    `db:"description"`
+	Status      string    `db:"status"`
+	Owner       string    `db:"owner"`
+	OwnerPath   string    `db:"owner_path"`
+	CreateTime  time.Time `db:"create_time"`
+	StatusTime  time.Time `db:"status_time"`
+	UpdateTime  time.Time `db:"update_time"`
+}
+
+func pbUserToDB(p *pb.User) *DBUser {
+	var q = &DBUser{
+		UserId:      p.UserId,
+		GroupId:     p.GroupId,
+		RoleId:      p.RoleId,
+		UserName:    p.UserName,
+		Position:    p.Position,
+		Email:       p.Email,
+		PhoneNumber: p.PhoneNumber,
+		Password:    p.Password,
+		OldPassword: p.OldPassword,
+		Description: p.Description,
+		Status:      p.Status,
+		Owner:       p.Owner,
+		OwnerPath:   p.OwnerPath,
+	}
+
+	q.CreateTime, _ = ptypes.Timestamp(p.CreateTime)
+	q.UpdateTime, _ = ptypes.Timestamp(p.UpdateTime)
+	q.StatusTime, _ = ptypes.Timestamp(p.StatusTime)
+
+	return q
+}
+
+func (p *DBUser) ToPb() *pb.User {
+	var q = &pb.User{
+		UserId:      p.UserId,
+		GroupId:     p.GroupId,
+		RoleId:      p.RoleId,
+		UserName:    p.UserName,
+		Position:    p.Position,
+		Email:       p.Email,
+		PhoneNumber: p.PhoneNumber,
+		Password:    p.Password,
+		OldPassword: p.OldPassword,
+		Description: p.Description,
+		Status:      p.Status,
+		Owner:       p.Owner,
+		OwnerPath:   p.OwnerPath,
+	}
+
+	q.CreateTime, _ = ptypes.TimestampProto(p.CreateTime)
+	q.UpdateTime, _ = ptypes.TimestampProto(p.UpdateTime)
+	q.StatusTime, _ = ptypes.TimestampProto(p.StatusTime)
+
+	return q
+}
