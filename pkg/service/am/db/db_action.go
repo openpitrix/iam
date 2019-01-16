0f227730266cbdda190c7ad7c5a3a67274cd2321
chai2010 <chaishushan@gmail.com>
2019-01-16 20:32:14 +0800

    refine code

--- pkg/service/am/db/db_action.go
+++ pkg/service/am/db/db_action.go
@@ -6,10 +6,35 @@ package db
 
 import (
 	"context"
+	"fmt"
 
-	pbam "openpitrix.io/iam/pkg/pb/am"
+	"openpitrix.io/logger"
+	"openpitrix.io/iam/pkg/pb/am"
+	"openpitrix.io/iam/pkg/service/am/db_spec"
 )
 
-func (p *Database) DescribeActions(context.Context, *pbam.DescribeActionsRequest) (*pbam.ActionList, error) {
-	panic("todo")
+func (p *Database) DescribeActions(ctx context.Context, req *pbam.DescribeActionsRequest) (*pbam.ActionList, error) {
+	// SELECT * FROM name
+	var query = fmt.Sprintf(
+		"SELECT * FROM %s;", db_spec.ActionTableName,
+	)
+
+	var rows = []db_spec.DBAction{}
+	err := p.DB.SelectContext(ctx, &rows, query)
+	if err != nil {
+		logger.Warnf(ctx, "%v", query)
+		logger.Warnf(ctx, "%+v", err)
+		return nil, err
+	}
+
+	var sets []*pbam.Action
+	for _, v := range rows {
+		sets = append(sets, v.ToPB())
+	}
+
+	reply := &pbam.ActionList{
+		Value: sets,
+	}
+
+	return reply, nil
 }
