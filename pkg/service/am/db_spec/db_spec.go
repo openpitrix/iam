85e1975736927eb1ec1c42b4ee6b5b429c315d91
chai2010 <chaishushan@gmail.com>
2019-01-16 15:22:15 +0800

    am: refine db spec

--- pkg/service/am/db_spec/db_spec.go
+++ pkg/service/am/db_spec/db_spec.go
@@ -13,200 +13,106 @@ var (
 )
 
 const (
-	UserTableName      = "user"
-	UserPrimaryKeyName = "user_id"
+	ActionTableName      = "action"
+	ActionPrimaryKeyName = "action_id"
 
-	UserGroupTableName      = "user_group"
-	UserGroupPrimaryKeyName = "group_id"
+	FeatureTableName      = "feature"
+	FeaturePrimaryKeyName = "feature_id"
 
-	UserGroupBindingTableName      = "user_group_binding"
-	UserGroupBindingPrimaryKeyName = "id"
+	ModuleTableName      = "module"
+	ModulePrimaryKeyName = "module_id"
+
+	RoleTableName      = "role"
+	RolePrimaryKeyName = "role_id"
+
+	RoleModuleBindingTableName      = "role_module_binding"
+	RoleModuleBindingPrimaryKeyName = "id"
+
+	UserRoleBindingTableName      = "user_role_binding"
+	UserRoleBindingPrimaryKeyName = "id"
 )
 
 var TableMap = map[string]struct{ Name, PrimaryKey, Sql string }{
-	UserTableName: {
-		Name:       UserTableName,
-		PrimaryKey: UserPrimaryKeyName,
-		Sql: `CREATE TABLE IF NOT EXISTS ` + UserTableName + ` (
-			user_id      varchar(50) not null,
-			user_name    varchar(50),
-			email        varchar(50),
-			phone_number varchar(50),
-			description  varchar(1000),
-			password     varchar(128),
-			status       varchar(10),
-			create_time  timestamp,
-			update_time  timestamp default CURRENT_TIMESTAMP,
-			status_time  timestamp,
-			extra        json,
+	ActionTableName: {
+		Name:       ActionTableName,
+		PrimaryKey: ActionPrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + ActionTableName + ` (
+			action_id   varchar(50) not null,
+			action_name varchar(50),
+			feature_id  varchar(50),
+			method      varchar(50),
+			description varchar(1000),
+			url         varchar(500),
+			url_method  varchar(20),
 
-			primary key(` + UserPrimaryKeyName + `)
+			primary key(` + ActionPrimaryKeyName + `)
 		);`,
 	},
-	UserGroupTableName: {
-		Name:       UserGroupTableName,
-		PrimaryKey: UserGroupPrimaryKeyName,
-		Sql: `CREATE TABLE IF NOT EXISTS ` + UserGroupTableName + ` (
-			group_id        varchar(50) not null,
-			group_path      varchar(255),
-			group_name      varchar(50),
-			description     varchar(1000),
-			status          varchar(10),
+	FeatureTableName: {
+		Name:       FeatureTableName,
+		PrimaryKey: FeaturePrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + FeatureTableName + ` (
+			feature_id   varchar(50) not null,
+			module_id    varchar(50),
+			feature_name varchar(50),
+
+			primary key(` + FeaturePrimaryKeyName + `)
+		);`,
+	},
+	ModuleTableName: {
+		Name:       ModuleTableName,
+		PrimaryKey: ModulePrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + ModuleTableName + ` (
+			module_id   varchar(50) not null,
+			module_name varchar(50),
+
+			primary key(` + ModulePrimaryKeyName + `)
+		);`,
+	},
+	RoleTableName: {
+		Name:       RoleTableName,
+		PrimaryKey: RolePrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + RoleTableName + ` (
+			role_id     varchar(50) not null,
+			role_name   varchar(50),
+			description varchar(255),
+			portal      varchar(50), -- 'admin,isv,dev,normal',
+			create_time timestamp,
+			update_time timestamp,
+			owner       varchar(50),
+			owner_path  varchar(50),
+
+			primary key(` + RolePrimaryKeyName + `)
+		);`,
+	},
+
+	// binding
+
+	RoleModuleBindingTableName: {
+		Name:       RoleModuleBindingTableName,
+		PrimaryKey: RoleModuleBindingPrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + RoleModuleBindingTableName + ` (
+			id              varchar(50) not null,
+			role_id         varchar(50),
+			module_id       varchar(50),
+			data_level      varchar(50),
+			enabled_actions text,
 			create_time     timestamp,
-			update_time  timestamp default CURRENT_TIMESTAMP,
-			status_time     timestamp,
-			extra           json,
+			update_time     timestamp,
+			owner           varchar(50),
 
-			parent_group_id varchar(50),
-			level           int,
-
-			primary key(` + UserGroupPrimaryKeyName + `)
+			primary key(` + RoleModuleBindingPrimaryKeyName + `)
 		);`,
 	},
-	UserGroupBindingTableName: {
-		Name:       UserGroupBindingTableName,
-		PrimaryKey: UserGroupBindingPrimaryKeyName,
-		Sql: `CREATE TABLE IF NOT EXISTS ` + UserGroupBindingTableName + ` (
-			id          varchar(50) not null,
-			user_id     varchar(50),
-			group_id    varchar(50),
-			create_time timestamp default CURRENT_TIMESTAMP,
+	UserRoleBindingTableName: {
+		Name:       UserRoleBindingTableName,
+		PrimaryKey: UserRoleBindingPrimaryKeyName,
+		Sql: `CREATE TABLE IF NOT EXISTS ` + UserRoleBindingTableName + ` (
+			id                   varchar(50) not null,
+			user_id              varchar(50),
+			role_id              varchar(50)
 
-			primary key(` + UserGroupBindingPrimaryKeyName + `)
+			primary key(` + UserRoleBindingPrimaryKeyName + `)
 		);`,
 	},
 }
-
-const sql = `
- -- create database am;
- -- use am;
-
-/*==============================================================*/
-/* Table: action                                                */
-/*==============================================================*/
-create table action
-(
-   action_id            varchar(50) not null,
-   feature_id           varchar(50),
-   action_name          varchar(50),
-   method               varchar(50),
-   description          varchar(100),
-   url                  varchar(500),
-   url_method           varchar(20)
-);
-
-alter table action
-   add primary key (action_id);
-
-/*==============================================================*/
-/* Table: action2                                               */
-/*==============================================================*/
-create table action2
-(
-   module_id            varchar(50),
-   module_name          varchar(50),
-   feature_id           varchar(50),
-   feature_name         varchar(50),
-   action_id            varchar(50) not null,
-   action_name          varchar(50),
-   method               varchar(50),
-   description          varchar(100),
-   url_method           varchar(20),
-   url                  varchar(500)
-);
-
-alter table action2
-   add primary key (action_id);
-
-/*==============================================================*/
-/* Table: feature                                               */
-/*==============================================================*/
-create table feature
-(
-   feature_id           varchar(50) not null,
-   module_id            varchar(50),
-   feature_name         varchar(50)
-);
-
-alter table feature
-   add primary key (feature_id);
-
-/*==============================================================*/
-/* Table: module                                                */
-/*==============================================================*/
-create table module
-(
-   module_id            varchar(50) not null,
-   module_name          varchar(50)
-);
-
-alter table module
-   add primary key (module_id);
-
-/*==============================================================*/
-/* Table: role                                                  */
-/*==============================================================*/
-create table role
-(
-   role_id              varchar(50) not null,
-   role_name            varchar(200),
-   description          varchar(255),
-   portal               varchar(50) comment ' admin,isv,dev,normal',
-   create_time          timestamp,
-   update_time          timestamp,
-   owner                varchar(50),
-   owner_path           varchar(50)
-);
-
-alter table role
-   add primary key (role_id);
-
-/*==============================================================*/
-/* Table: role_module_binding                                   */
-/*==============================================================*/
-create table role_module_binding
-(
-   id                   varchar(50) not null,
-   role_id              varchar(50),
-   module_id            varchar(50),
-   data_level           varchar(50) comment 'all,department,onlyself',
-   enabled_actions      text comment 'enabled_actions includes specific actions��$* means all the actions under the module_id, if not, it should be action_id strings with comma',
-   create_time          timestamp,
-   update_time          timestamp,
-   owner                varchar(50)
-);
-
-alter table role_module_binding
-   add primary key (id);
-
-/*==============================================================*/
-/* Table: user_role_binding                                     */
-/*==============================================================*/
-create table user_role_binding
-(
-   id                   varchar(50) not null,
-   user_id              varchar(50),
-   role_id              varchar(50)
-);
-
-alter table user_role_binding
-   add primary key (id);
-
--- alter table action add constraint FK_Reference_10 foreign key (feature_id)
---       references feature (feature_id) on delete restrict on update restrict;
---
--- alter table feature add constraint FK_Reference_8 foreign key (module_id)
---       references module (module_id) on delete restrict on update restrict;
---
--- alter table role_module_binding add constraint FK_Reference_11 foreign key (module_id)
---       references module (module_id) on delete restrict on update restrict;
---
--- alter table role_module_binding add constraint FK_Reference_5 foreign key (role_id)
---       references role (role_id) on delete restrict on update restrict;
---
--- alter table user_role_binding add constraint FK_Reference_19 foreign key (user_id)
---       references user (user_id) on delete restrict on update restrict;
---
--- alter table user_role_binding add constraint FK_Reference_20 foreign key (role_id)
---       references role (role_id) on delete restrict on update restrict;
-`
