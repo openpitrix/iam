2a59717154f0b07b08dc291a98ede4b54b67fda5
chai2010 <chaishushan@gmail.com>
2019-01-20 13:56:44 +0800

    am: refine sql

--- pkg/service/am/db/db_action_sql.go
+++ pkg/service/am/db/db_action_sql.go
@@ -4,7 +4,7 @@
 
 package db
 
-const sqlDescribeActionsBy_RoleId_Protal = `
+const sqlDescribeActionsBy_RoleId_Protal_old = `
 -- argument[0]: role_id
 -- argument[1]: portal
 select distinct
@@ -44,17 +44,30 @@ order by
 	t3.action_id
 `
 
-const sqlDescribeActionsBy_RoleId_Protal_new = `
+const sqlDescribeActionsBy_RoleId_Protal = `
+-- argument[0]: role_id
+-- argument[1]: portal
 select distinct
-distinct t3.role_id,t3.role_name,t3.portal,t1.module_id,t1.module_name,
-t1.feature_id,t1.feature_name,t2.data_level,
-t1.action_id,
-(case when isnull(t4.action_id)=0 then 'true' else 'false' end) as action_enabled,
-t1.action_name ,t1.api_id,t1.api_method,
-t1.url_method,t1.url
-from action2 t1
-left join role_module_binding t2 on t1.module_id=t2.module_id
-left join role t3 on t2.role_id=t3.role_id  and t3.role_id='role_0002'
-left join enable_action t4 on t4.action_id= t1.action_id
-where   t3.portal='isv'
+	t3.role_id,
+	t3.role_name,
+	t3.portal,
+	t1.module_id,
+	t1.module_name,
+	t1.feature_id,
+	t1.feature_name,
+	t2.data_level,
+	t1.action_id,
+	(case when isnull(t4.action_id)=0 then 'true' else 'false' end) as action_enabled,
+	t1.action_name,
+	t1.api_id,
+	t1.api_method,
+	t1.url_method,
+	t1.url
+from
+	action2 t1
+	left join role_module_binding t2 on t1.module_id=t2.module_id
+	left join role t3 on t2.role_id=t3.role_id and t3.role_id=?
+	left join enable_action t4 on t4.action_id= t1.action_id
+where
+	t3.portal=?
 `
