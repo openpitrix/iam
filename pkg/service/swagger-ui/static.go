066f05c9c4fa40377987a24bd192021604bafbe5
chai2010 <chaishushan@gmail.com>
2019-01-25 23:04:05 +0800

    api: refine api

--- pkg/service/swagger-ui/static.go
+++ pkg/service/swagger-ui/static.go
@@ -819,6 +819,12 @@ var K=n(514),X=n(517),Z=n(518);t.Buffer=o,t.SlowBuffer=m,t.INSPECT_MAX_BYTES=50,
         "status_time": {
           "type": "string",
           "format": "date-time"
+        },
+        "user_id": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
         }
       }
     },
